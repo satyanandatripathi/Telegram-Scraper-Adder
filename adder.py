c5644a079d04ed34c094ec5b820cff904c635c7e
satyanandatripathi <83153745+satyanandatripathi@users.noreply.github.com>
2021-05-06 11:26:02 +0530

    Update adder.py

--- adder.py
+++ adder.py
@@ -20,8 +20,8 @@ print (gr+" ║ ├┤ │  ├┤ ║ ╦  ╠═╣ ││ ││├┤ ├┬
 print (re+" ╩ └─┘┴─┘└─┘╚═╝  ╩ ╩─┴┘─┴┘└─┘┴└─")
 
 print (cy+"version : 1.01")
-print (cy+"Make sure you Subscribed Termux Professor On Youtube")
-print (cy+"www.youtube.com/c/TermuxProfessorYT")
+print (cy+"Make sure to join our Telegram channel")
+print (cy+"https://t.me/joinchat/OxmYRwSJN9ozMGY1")
 
 print (re+"NOTE :")
 print ("1. Telegram only allow to add 200 members in group by one user.")
@@ -29,6 +29,7 @@ print ("2. You can Use multiple Telegram accounts for add more members.")
 print ("3. Add only 50 members in group each time otherwise you will get flood error.")
 print ("4. Then wait for 15-30 miniute then add members again.")
 print ("5. Make sure you enable Add User Permission in your group")
+print ("6. stay connected with satyanand TRiPATHi for more like this credit goes to termux professor.")
 
 cpass = configparser.RawConfigParser()
 cpass.read('config.data')
